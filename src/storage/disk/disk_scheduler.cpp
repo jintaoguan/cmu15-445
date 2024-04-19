b2d8b4f05338d638562e1690a1d706313858086e
Jintao Guan <jintaoguan0817@gmail.com>
2024-04-19 02:52:08 -0700

    2023-fall project#1 task#2

--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -11,17 +11,13 @@
 //===----------------------------------------------------------------------===//
 
 #include "storage/disk/disk_scheduler.h"
+#include <optional>
 #include "common/exception.h"
 #include "storage/disk/disk_manager.h"
 
 namespace bustub {
 
 DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
-  // TODO(P1): remove this line after you have implemented the disk scheduler API
-  throw NotImplementedException(
-      "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
-      "throw exception line in `disk_scheduler.cpp`.");
-
   // Spawn the background thread
   background_thread_.emplace([&] { StartWorkerThread(); });
 }
@@ -34,8 +30,30 @@ DiskScheduler::~DiskScheduler() {
   }
 }
 
-void DiskScheduler::Schedule(DiskRequest r) {}
+void DiskScheduler::Schedule(DiskRequest r) {
+  auto request = std::make_optional<DiskRequest>(std::move(r));
+  request_queue_.Put(std::move(request));
+}
 
-void DiskScheduler::StartWorkerThread() {}
+void DiskScheduler::StartWorkerThread() {
+  auto request = request_queue_.Get();
+  while (true) {
+    if (!request.has_value()) {
+      break;
+    }
+    auto is_write = request->is_write_;
+    auto page_id = request->page_id_;
+    auto data = request->data_;
+    auto callback = std::move(request->callback_);
+
+    if (is_write) {
+      disk_manager_->WritePage(page_id, data);
+    } else {
+      disk_manager_->ReadPage(page_id, data);
+    }
+    callback.set_value(true);
+    request = request_queue_.Get();
+  }
+}
 
 }  // namespace bustub
