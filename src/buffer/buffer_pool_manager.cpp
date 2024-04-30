65bbea23eb558bff91fdfe2abf2ef907b8f46d4d
Jintao Guan <jintaoguan0817@gmail.com>
2024-04-29 23:02:57 -0700

    2023-fall project#2 task#1

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -239,12 +239,30 @@ auto BufferPoolManager::DeletePage(page_id_t page_id) -> bool {
 
 auto BufferPoolManager::AllocatePage() -> page_id_t { return next_page_id_++; }
 
-auto BufferPoolManager::FetchPageBasic(page_id_t page_id) -> BasicPageGuard { return {this, nullptr}; }
+auto BufferPoolManager::FetchPageBasic(page_id_t page_id) -> BasicPageGuard {
+  auto fetched_page = FetchPage(page_id);
+  return BasicPageGuard(this, fetched_page);
+}
 
-auto BufferPoolManager::FetchPageRead(page_id_t page_id) -> ReadPageGuard { return {this, nullptr}; }
+auto BufferPoolManager::FetchPageRead(page_id_t page_id) -> ReadPageGuard {
+  auto fetched_page = FetchPage(page_id);
+  if (fetched_page != nullptr) {
+    fetched_page->RLatch();
+  }
+  return ReadPageGuard(this, fetched_page);
+}
 
-auto BufferPoolManager::FetchPageWrite(page_id_t page_id) -> WritePageGuard { return {this, nullptr}; }
+auto BufferPoolManager::FetchPageWrite(page_id_t page_id) -> WritePageGuard {
+  auto fetched_page = FetchPage(page_id);
+  if (fetched_page != nullptr) {
+    fetched_page->WLatch();
+  }
+  return WritePageGuard(this, fetched_page);
+}
 
-auto BufferPoolManager::NewPageGuarded(page_id_t *page_id) -> BasicPageGuard { return {this, nullptr}; }
+auto BufferPoolManager::NewPageGuarded(page_id_t *page_id) -> BasicPageGuard {
+  auto new_page = NewPage(page_id);
+  return BasicPageGuard{this, new_page};
+}
 
 }  // namespace bustub
